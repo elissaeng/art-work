2787cd09e4a24147b0cba4992bf35a80abe507cd
elissa <elissaeng@Elissas-MacBook-Pro.local>
2021-06-17 17:05:27 -0700

    artist_edit created and working

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,9 +4,11 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('artist', views.artist_index, name='artist_index'),
-  path('artist/<int:artist_id>', views.artist_show, name='artist_show'),
+  path('artist/<int:artist_id>/', views.artist_show, name='artist_show'),
   path('artist/new/', views.artist_create, name='artist_create'),
+  path('artist/edit/<int:artist_id>/', views.artist_edit, name='artist_edit'),
 
-  path('gallery', views.gallery_index, name='gallery_index'),
-  path('gallery/<int:gallery_id>', views.gallery_show, name='gallery_show'),
+
+  path('gallery/', views.gallery_index, name='gallery_index'),
+  path('gallery/<int:gallery_id>/', views.gallery_show, name='gallery_show'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Artist, Gallery
 from .forms import ArtistForm
 
@@ -10,7 +10,8 @@ def home(request):
 # ARTIST INDEX
 def artist_index(request):
   artists = Artist.objects.all()
-  return render(request, 'artists/artist_index.html')
+  context = { 'artists': artists }
+  return render(request, 'artists/artist_index.html', context)
 
 
 # ARTIST SHOW
@@ -22,7 +23,6 @@ def artist_show(request, artist_id):
   return render(request, 'artists/artist_show.html', context)
 
 
-
 # CREATE ARTIST
 def artist_create(request):
   if request.method == 'GET':
@@ -31,15 +31,32 @@ def artist_create(request):
       'form': form,
     }
     return render(request, 'artists/artist_create.html', context)
-  # else:
-  #   form = ArtistForm(request.POST)
-  #   if form.is_valid():
-  #     artist = form.save(commit=False)
-  #     artist.user = request.user
-  #     artist.save()
-  #     return redirect('artist_show', artist.id)
+  else:
+    form = ArtistForm(request.POST)
+    if form.is_valid():
+      artist = form.save(commit=False)
+      artist.user = request.user
+      artist.save()
+      return redirect('artist_show', artist.id)
 
 
+# EDIT ARTIST
+def artist_edit(request, artist_id):
+  artist = Artist.objects.get(id=artist_id)
+
+  if request.method == 'GET':
+    form = ArtistForm(instance=artist)
+    context = {
+    'form': form
+  }
+    return render(request, 'artists/artist_edit.html', context)
+  else:
+    form = ArtistForm(request.POST, instance=artist)
+    if form.is_valid():
+      artist = form.save()
+      return redirect('artist_show', artist.id)
+
+     
 
 # GALLERY INDEX
 def gallery_index(request):
